0b97612ee5ca3057b0cedb3d24efbd33926548f5
Uxoi <jhojanproxdd@gmail.com>
2024-06-17 13:52:17 -0500

    New features added.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-const ERXClient = require("./classes/client")
-const text = require("./functions/random/text")
-const number = require("./functions/random/number")
+const ERXClient = require('./classes/client');
+const text = require('./functions/random/text');
+const number = require('./functions/random/number');
+const intents = require('./intents/intents');
+const argument = require('./functions/message/argument');
+const send = require('./functions/message/sendMessage');
+const id = require("./functions/client/id");
+const username = require("./functions/user/username")
+const authorId = require("./functions/user/authorId");
 
 module.exports = {
     ERXClient,
+    Intents: intents,
     cmd: {
         random: {
             text,
             number
+        },
+        message: {
+            argument,
+            send
+        },
+        client: {
+            id
+        },
+        user: {
+            username,
+            authorId
         }
     }
-}
+};
